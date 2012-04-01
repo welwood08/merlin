dcc68f1646b72e71d1c5b4281899955287a23875
vuLgAr <vulgarmessiah@gmail.com>
2012-04-01 17:55:25 +0100

    source website link from config

--- Hooks/newb.py
+++ Hooks/newb.py
@@ -1,9 +1,11 @@
 from Core.loadable import loadable, route
+from Core.config import Config
 
 class newb(loadable):
-
+    
     @route()
     def execute(self, message, user, params):
-
-        webby = ["lolidunno.co.uk",]
-        message.reply("PM me and set !pref planet=*:*:* and !pref password=***** then you can sign into "+" | ".join(webby))
+        
+        links = [Config.get("URL","arthur"),]
+        
+        message.reply(self.url("Sign up to netgamers.org, Log into P and set usermode +ix - Do !pref password=*** and !pref planet=*:*:* IN A PM TO ME! You can then sign into: "+" | ".join(links), user))
