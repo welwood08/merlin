90ede6b47924e987b42e291ef37cf4c4f911254c
Martin Stone <martin@d7415.co.uk>
2012-10-17 17:52:38 +0100

    Changed flood protection to one line every 2 seconds.
    Signed-off-by: Martin Stone <martin@d7415.co.uk>

--- Core/connection.py
+++ Core/connection.py
@@ -88,7 +88,7 @@ class connection(object):
             if ponging:
                 self.sock.send(line + CRLF)
             else:
-                while self.last + 1 >= time.time():
+                while self.last + 2 >= time.time():
                     time.sleep(0.5)
                 self.sock.send(encode(line) + CRLF)
                 self.last = time.time()
