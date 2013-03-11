2f94931e32d2510e972db18d02fca5d35e11f35a
Martin Stone <martin@d7415.co.uk>
2013-03-11 20:52:56 +0000

    Added Group.admin_only to migration in createdb.py Signed-off-by: Martin Stone <martin@d7415.co.uk>

--- createdb.py
+++ createdb.py
@@ -126,7 +126,7 @@ if round and not mysql:
         session.execute(text("INSERT INTO %susers (id, name, alias, passwd, active, group_id, url, email, phone, pubphone, _smsmode, sponsor, quits, available_cookies, carebears, last_cookie_date, fleetcount) SELECT id, name, alias, passwd, active, group_id, url, email, phone, pubphone, _smsmode::varchar::smsmode, sponsor, quits, available_cookies, carebears, last_cookie_date, 0 FROM %s.%susers;" % (prefix, round, old_prefix)))
         session.execute(text("SELECT setval('%susers_id_seq',(SELECT max(id) FROM %susers));" % (prefix, prefix)))
         session.execute(text("INSERT INTO %sphonefriends (user_id, friend_id) SELECT user_id, friend_id FROM %s.%sphonefriends;" % (prefix, round, old_prefix)))
-        session.execute(text("INSERT INTO %sgroup (id, name, desc) SELECT id, name, desc FROM %s.%sgroup;" % (prefix, round, old_prefix)))
+        session.execute(text("INSERT INTO %sgroup (id, name, desc, admin_only) SELECT id, name, desc, admin_only FROM %s.%sgroup;" % (prefix, round, old_prefix)))
         session.execute(text("INSERT INTO %saccess (id, group_id) SELECT id, group_id FROM %s.%saccess;" % (prefix, round, old_prefix)))
         print "  - slogans/quotes"
         session.execute(text("INSERT INTO %sslogans (text) SELECT text FROM %s.%sslogans;" % (prefix, round, old_prefix)))
