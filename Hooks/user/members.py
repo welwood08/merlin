8bd15044d26ad6d445602af079fbb5c5616bb2c8
Martin Stone <martin@d7415.co.uk>
2013-03-12 21:07:33 +0000

    !members: Updated for new access system.
    Signed-off-by: Martin Stone <martin@d7415.co.uk>

--- Hooks/user/members.py
+++ Hooks/user/members.py
@@ -23,13 +23,12 @@
  
 from sqlalchemy.sql import asc
 from Core.db import session
-from Core.maps import User, Updates
+from Core.maps import Group, Updates
 from Core.loadable import loadable, route
-from Core.config import Config
 
 class members(loadable):
     """List all members, in format nick (alias). Optionally include coordinates, mydef age, tick of last mydef update."""
-    usage = " [coords] [defage] [mydef] [galmates]"
+    usage = " [coords] [defage] [mydef] [galmates] [showempty]"
     access = 1 # Admin
     
     @route(r"(.*)", access = "members")
@@ -37,20 +36,15 @@ class members(loadable):
         reply = ""
         tick=Updates.current_tick()
         opts = params.group(1).split()
-        for o in reversed(Config.options("Access")):
-            if ("galmates" not in opts) and (Config.getint("Access", o) == 0):
+
+        for g in session.query(Group).order_by(asc(Group.name)).all():
+            if ("galmates" not in opts) and (g.id == 2):
                 continue;
-            Q = session.query(User)
-            Q = Q.filter(User.access == Config.getint("Access", o))
-            Q = Q.order_by(asc(User.name))
-            result = Q.all()
-            if len(result) < 1:
+            if len(g.users) < 1 and "showempty" not in opts:
                 continue
             printable=map(lambda (u): "%s%s%s%s%s" % (u.name,' ('+u.alias+')' if u.alias else '',
                 " (%d:%d:%d)" % (u.planet.x, u.planet.y, u.planet.z) if "coords" in opts and u.planet is not None else '', 
                 " (%s)" % ((u.fleetupdated or 0)-tick) if "defage" in opts else '',
-                " (%s)" % (u.fleetupdated or 0) if "mydef" in opts else ''),result)
-            reply += "%s:  " % (o)
-            reply += ', '.join(printable)
-            reply += '\n'
+                " (%s)" % (u.fleetupdated or 0) if "mydef" in opts else ''),g.users)
+            reply += "%s:  %s\n" % (g.name, ', '.join(printable))
         message.reply(reply[:-1])
