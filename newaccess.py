c65c91866cf9f0310467ee8f2ee9553ae8849f5c
Martin Stone <martin@d7415.co.uk>
2013-03-15 20:33:43 +0000

    newaccess.py script to check commands / subcommands for any new access levels.
    Signed-off-by: Martin Stone <martin@d7415.co.uk>

---
+++ newaccess.py
@@ -0,0 +1,62 @@
+# This file is part of Merlin.
+# Merlin is the Copyright (C)2008,2009,2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
+
+# Individual portions may be copyright by individual contributors, and
+# are included in this collective work with permission of the copyright
+# owners.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+ 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+ 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Script by Martin Stone
+ 
+from Core.db import session
+from Core.maps import Group, Access
+from Core.callbacks import Callbacks
+
+print "Looking for new commands and subcommands..."
+def addaccess(name, access):
+    command = Access.load(name)
+    if command:
+        return
+
+    command = Access(name=name)
+    session.add(command)
+    if access == 2:
+        command.groups.append(Group(id=2))
+        command.groups.append(Group(id=3))
+        command.groups.append(Group(id=4))
+    if access == 3:
+        command.groups.append(Group(id=3))
+        command.groups.append(Group(id=4))
+    elif access != 1:
+        command.groups.append(Group(id=access))
+
+
+for callback in Callbacks.callbacks['PRIVMSG']:
+    if not callback.access:
+        continue
+    addaccess(callback.name, callback.access)
+    try:
+       if callback.subcommands:
+           i = 0
+           while i < len(callback.subcommands):
+               addaccess(callback.subcommands[i], callback.subaccess[i])
+               i += 1
+    except:
+        pass
+    session.commit()
+
+session.close()
+print "Done!"
