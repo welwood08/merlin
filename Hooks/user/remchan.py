f358f24c6b55d1cbd04d13a97a3702566440668f
Martin Stone <martin@d7415.co.uk>
2013-03-24 17:48:33 +0000

    !remchan: Uncommented importing Config after adding the "autoinvite off" line. Signed-off-by: Martin Stone <martin@d7415.co.uk>

--- Hooks/user/remchan.py
+++ Hooks/user/remchan.py
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-#from Core.config import Config
+from Core.config import Config
 from Core.db import session
 from Core.maps import Channel
 from Core.loadable import loadable, route, require_user
