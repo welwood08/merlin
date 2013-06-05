d1a5ea2d70571a69b4967cf75272b3803eee6dd9
Martin Stone <martin@d7415.co.uk>
2013-06-05 23:06:14 +0100

    excalibur: Made older-python-friendly. (Make fewer assumptions about errors)
    Signed-off-by: Martin Stone <martin@d7415.co.uk>
    (cherry picked from commit ff27d19caa7a4db4b44932c301a668bb68101b72)

--- excalibur.pg.py
+++ excalibur.pg.py
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import datetime, re, sys, time, traceback, urllib2, shutil, os
+import datetime, re, sys, time, traceback, urllib2, shutil, os, errno
 from sqlalchemy.sql import text, bindparam
 from Core.config import Config
 from Core.paconf import PA
@@ -231,7 +231,11 @@ def ticker(alt=False):
             modified = planets.headers.get("Last-Modified")
     
             if savedumps:
-                os.makedirs("dumps/%s" % (last_tick+1,))
+                try:
+                    os.makedirs("dumps/%s" % (last_tick+1,))
+                except OSError as e:
+                    if e.errno != errno.EEXIST:
+                        raise
                 # Open dump files
                 pf = open("dumps/%s/planet_listing.txt" % (last_tick+1,), "w+")
                 gf = open("dumps/%s/galaxy_listing.txt" % (last_tick+1,), "w+")
