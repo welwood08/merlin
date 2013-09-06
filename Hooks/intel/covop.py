c6c33e86b570a85da520c34abf9bc7909d0cfcb2
Martin Stone <martin@d7415.co.uk>
2013-09-06 22:24:26 +0100

    !covop: Add Government Subversion covop.
    Signed-off-by: Martin Stone <martin@d7415.co.uk>

--- Hooks/intel/covop.py
+++ Hooks/intel/covop.py
@@ -84,8 +84,9 @@ class covop(loadable):
             max_res = user.planet.resources_per_agent(planet)
             message.reply("Results:   SD: %1.1f%% (%dXP) NC: %d%% (%dXP) EF: %d roids (%dXP) S: %d ships (%dXP) SGP: %d guards (%dXP)" % (0.5*agents, self.xpcalc(agents,1),
                      agents, self.xpcalc(agents,2), agents/3, self.xpcalc(agents,3), agents, self.xpcalc(agents,4), 10*agents, self.xpcalc(agents,5)))
-            message.reply("           IB: %d amps+dists (%dXP) RT: %dM %dC %dE (%dXP) H: %d buildings (%dXP)" % (agents/15, self.xpcalc(agents,6),
-                     min(max_res, pscan.res_metal/10), min(max_res, pscan.res_crystal/10), min(max_res, pscan.res_eonium/10), self.xpcalc(agents,7), agents/20, self.xpcalc(agents,8)))
+            message.reply("           IB: %d amps+dists (%dXP) RT: %dM %dC %dE (%dXP) H: %d buildings (%dXP) GS: %d ticks (%dXP)" % (agents/15, self.xpcalc(agents,6),
+                     min(max_res, pscan.res_metal/10), min(max_res, pscan.res_crystal/10), min(max_res, pscan.res_eonium/10), self.xpcalc(agents,7), agents/20,
+                     self.xpcalc(agents,8), agents/5, self.xpcalc(agents,9)))
 
             if params.group(7):
                 stealth = int(params.group(7))
