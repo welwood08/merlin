f26124a38095b303c8dec2d4df5a38478e2096fd
Martin Stone <martin@d7415.co.uk>
2013-12-14 18:35:22 +0000

    Core/maps: Add Twilio smsmode.
    Signed-off-by: Martin Stone <martin@d7415.co.uk>
    (cherry picked from commit 772d8dfcbd5eb1b3afb0a186acbf9357c09cd1c9)

--- Core/maps.py
+++ Core/maps.py
@@ -1205,7 +1205,7 @@ Group.commands = relation(Access, secondary=(grants), lazy='dynamic', backref="g
 
 class User(Base):
     __tablename__ = Config.get('DB', 'prefix') + 'users'
-    _sms_modes = {"C":"Clickatell", "G":"GoogleVoice", "R":"Retard", "E":"Email", "W":"WhatsApp",}
+    _sms_modes = {"C":"Clickatell", "G":"GoogleVoice", "R":"Retard", "E":"Email", "W":"WhatsApp", "T":"Twilio",}
     id = Column(Integer, primary_key=True)
     name = Column(String(255)) # pnick
     alias = Column(String(255))
