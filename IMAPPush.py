36160a35d3f14b386ab202bbb45c3a781b23b30a
Martin Stone <martin@d7415.co.uk>
2014-01-15 16:21:40 +0000

    IMAPPush: Make IMAP username case-insensitive (only affects singleaddr users).
    Signed-off-by: Martin Stone <martin@d7415.co.uk>
    (cherry picked from commit 5a4ba33700af5d7fa7e98cf3959dfd2ddbddb344)

--- IMAPPush.py
+++ IMAPPush.py
@@ -120,10 +120,10 @@ class Idler(threading.Thread):
     def robonotify(self, header, body):
         # Check for correct "From" address?
         if Config.getboolean("imap", "singleaddr"):
-            uname_re = "%s\+(.+)@.+" % Config.get("imap", "user").split("@")[0]
+            uname_re = "%s\+(.+)@.+" % Config.get("imap", "user").split("@")[0].lower()
         else:
             uname_re = "<?(.+)@.+"
-        uname = re.findall(uname_re, header['To'])[0]
+        uname = re.findall(uname_re, header['To'].lower())[0]
 
         dsuff = Config.get("imap", "defsuffix")
         if dsuff:
