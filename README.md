c580977486b7168ac573947527f6650ba2ff54ba
Martin Stone <martin@d7415.co.uk>
2015-05-06 17:51:42 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Userscript
+==========
+This is a **very** simple userscript for automatically submitting scan URLs to merlin. It does this by submitting the current URL to Arthur whenever a scan page is viewed. It is likely to be replaced with a better (and very different) version in the near future.
---
+++ merlin_scans.user.js
@@ -0,0 +1,35 @@
+﻿// ==UserScript==
+// @name           Merlin Scan Script for YOUR_ALLIANCE
+// @version        2.0.0
+// @namespace      https://github.com/d7415/merlin
+// @description    Submit scan URLs to Arthur.
+// @grant          unsafeWindow
+
+// @exclude        *://*/login*
+// @exclude        *://*/signup*
+// @exclude        *://*/register*
+// @exclude        *://*/preferences*
+// @exclude        *://*/support*
+// @exclude        *://*/manual*
+// @exclude        *://*/chat*
+// @exclude        *://*/forum*
+// @exclude        *://*/botfiles*
+// @exclude        *://beta.planetarion.com/*
+// @exclude        *://speedgame.planetarion.com/*
+// @exclude        *://pirate.planetarion.com/*
+// @exclude        *://ninja.planetarion.com/*
+// @exclude        *://forum.planetarion.com/*
+// @exclude        *://www.planetarion.com/*
+
+// @include        /^https?://146\.185\.135\.215/(waves|showscan).pl\?/
+// @include        /^https?://(pa|game)\.ranultech\.co\.uk/(waves|showscan).pl\?/
+// @include        /^https?://[^/]*\.planetarion\.com/(waves|showscan).pl\?/
+// @copyright      2008-2012 William Elwood (Will) / 2012 Dizkarte / 2013-2015 Martin Stone (Pit)
+// @license        GNU General Public License version 3; http://www.gnu.org/licenses/gpl.html
+// ==/UserScript==
+
+(function(window, doc) {
+    var script = doc.createElement('script');
+    script.setAttribute('src', 'http://ARTHUR_DOMAIN_HERE/lookup/?nick=' + unsafeWindow.PA_planet.nick + '&lookup=' + window.location);
+    doc.body.appendChild(script);
+})(this, this.document);
